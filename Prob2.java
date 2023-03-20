889639b3efefe5595454d09ec5665b927278434f
Sohan029 <121715317+Sohan029@users.noreply.github.com>
2023-03-20 14:52:34 +0530

    Prob2.java

---
+++ Prob2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class oddSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int count=0;
+        for(int i=0;i<n;i++){
+            int sum=0;
+            for(int j=i;j<n;j++){
+                sum+=arr[j];
+                if(sum%2==1){
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
